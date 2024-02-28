7b2bff1e517f1b36aaa70668000e68dacb275732
NuraddinMammadzada <nmammadzada64@gmail.com>
2024-02-28 17:14:53 +0400

    finish

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// const bucaq = Number(prompt("Zəhmət olmasa bucaqi daxil edin:"));
+// let bucaq = Number(prompt("Zəhmət olmasa bucaqi daxil edin:"));
 
 // let message;
 // if (bucaq < 90) {
@@ -13,7 +13,7 @@
 
 // alert(message);
 
-// const bucaq = Numnber(prompt("Zəhmət olmasa bucaqi daxil edin:"));
+// let bucaq = Numnber(prompt("Zəhmət olmasa bucaqi daxil edin:"));
 
 // alert(
 //     bucaq < 90 ? "İti bucaq" : bucaq === 90 ? "Düz bucaq" : bucaq < 180 ? "Kor bucaq" : "Açıq bucaq"
