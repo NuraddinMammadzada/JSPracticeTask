7a51943fa692b6e8c25d5a848023280ade7cfef9
NuraddinMammadzada <nmammadzada64@gmail.com>
2024-02-28 17:10:04 +0400

    2 variant has ended

---
+++ main.js
@@ -0,0 +1,21 @@
+// const bucaq = parseInt(prompt("Zəhmət olmasa açını daxil edin:"));
+
+// let message;
+// if (bucaq < 90) {
+//     message = "İti bucaq";
+// } else if (bucaq === 90) {
+//     message = "Düz bucaq";
+// } else if (bucaq < 180) {
+//     message = "Kor bucaq";
+// } else if (bucaq == 180) {
+//     message = "Açıq bucaq";
+// }
+
+// alert(message);
+
+// const bucaq = parseInt(prompt("Zəhmət olmasa açını daxil edin:"));
+
+// alert(
+//     bucaq < 90 ? "İti bucaq" : bucaq === 90 ? "Düz bucaq" : bucaq < 180 ? "Kor bucaq" : "Açıq bucaq"
+// );
+
